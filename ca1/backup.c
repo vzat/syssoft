d7f5d584471a6d050f5805fd7d1187f8bafe7052
Vlad Zat <vlad.zat@mydit.ie>
2018-02-27 11:55:51 +0000

    Move block/allow access and backup to dedicated dir

--- ca1/backup.c
+++ ca1/backup.c
@@ -4,9 +4,9 @@
 #include <sys/wait.h>
 #include <string.h>
 
-#define INTRANET "/var/www/html/intranet"
-#define LIVE "/var/www/html/live"
-#define BACKUP "/var/www/html/backup"
+#define INTRANET "/var/www/html/intranet/"
+#define LIVE "/var/www/html/live/"
+#define BACKUP "/var/www/html/backup/"
 
 void blockAccess() {
     printf("Blocking access to files\n\n");
@@ -20,18 +20,11 @@ void blockAccess() {
 void backup() {
     printf("Backing up LIVE\n\n");
 
+    // Trailing slash for the directories is necessary to copy just the contents!!!
     execlp("rsync", "rsync", "-avP", LIVE, BACKUP, NULL);
 
     perror("Cannot backup files");
     exit(1);
-
-    // FILE *fp;
-    //
-    // fp = open('/var/www/html/liveUpdates.txt', "r");
-    //
-    // while (fgets(path, sizeof(path), fp) != NULL) {
-    //     printf("%s", path);
-    // }
 }
 
 void unblockAccess() {
@@ -44,50 +37,62 @@ void unblockAccess() {
 }
 
 void backupAndTransfer() {
-    // This should be in a fork called from main
-    // Fork for each process?
-    // Steps:
-    // 1. blockAccess
-    // 2. backup
-    // 3. transfer
-    // 4. unblockAccess
+    int status;
+
+    switch (fork()) {
+        case 0:
+            blockAccess();
+        case -1:
+            perror("Error init fork");
+            exit(1);
+        default:
+            break;
+    }
+
+    wait(&status);
+
+    switch (fork()) {
+        case 0:
+            backup();
+        case -1:
+            perror("Error init fork");
+            exit(1);
+        default:
+            break;
+    }
+
+    wait(&status);
+
+    switch (fork()) {
+        case 0:
+            unblockAccess();
+        case -1:
+            perror("Error init fork");
+            exit(1);
+        default:
+            break;
+    }
 }
 
 int main(void) {
     pid_t pid;
     int status;
 
-    if ((pid = fork()) == -1) {
-        perror("Error init fork");
-        exit(1);
-    }
-    else if (pid == 0) {
-        blockAccess();
-    }
-
-    wait(&status);
-
-    if ((pid = fork()) == -1) {
-        perror("Error init fork");
-        exit(1);
-    }
-    else if (pid == 0) {
-        backup();
-    }
-
-    wait(&status);
-
-    if ((pid = fork()) == -1) {
-        perror("Error init fork");
-        exit(1);
-    }
-    else if (pid == 0) {
-        unblockAccess();
-    }
-
-    while (true) {
+    while (1) {
         // Check if it's 2am ---> backup/transfer
         // Check if manual update was triggered ---> backup/transfer
+
+        if ((pid = fork()) == -1) {
+            perror("Error init fork");
+            exit(1);
+        }
+        else if (pid == 0) {
+            backupAndTransfer();
+        }
+
+        wait(&status);
+
+        getchar();
     }
 
     return 0;

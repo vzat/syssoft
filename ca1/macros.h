02ad7eb39a52c6a6832f762e0baa7f6f258499de
Vlad Zat <vlad.zat@mydit.ie>
2018-03-09 11:54:49 +0000

    CA - Add message queue

--- ca1/macros.h
+++ ca1/macros.h
@@ -1,5 +1,8 @@
 #define INTRANET "/var/www/html/intranet/"
 #define LIVE "/var/www/html/live/"
 #define BACKUP "/var/www/html/backup/"
+#define QUEUE_NAME "/daemonQ"
 #define AUDIT_KEY "intranet.audit"
+
+#define MAX_BUF 1024
 #define MAX_DATE 1024
--- ca1/main.c
+++ ca1/main.c
@@ -2,27 +2,66 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <mqueue.h>
+#include <syslog.h>
 
 #include "macros.h"
 #include "timelib.h"
 
-int main(void) {
-    // Create orphan to run the scheduler
-    // Create orphan for the daemon
-    // They should talk using a fifofile
+int main (int argc, char **argv) {
 
-    // size_t dateSize = sizeof(char) * MAX_DATE;
-    // char * now = malloc(dateSize);
-    //
-    // getCurrentTime(now, dateSize);
-    // printf("Current Date: %s\n", now);
-    //
-    // waitForTime(15, 30, 0);
-    //
-    // getCurrentTime(now, dateSize);
-    // printf("Current Date: %s\n", now);
-    //
-    // free(now);
+    // TODO: Create orphan process to deamonize this process
+
+    mqd_t mq;
+
+    struct mq_attr queue_attributes;
+    char buffer[MAX_BUF + 1];
+
+    int terminate = 0;
+
+    // Message Queue Attributes
+    queue_attributes.mq_flags = 0;
+    queue_attributes.mq_maxmsg = 10;
+    queue_attributes.mq_msgsize = MAX_BUF;
+    queue_attributes.mq_curmsgs = 0;
+
+    // Create log
+    openlog("serverdaemon", LOG_PID | LOG_CONS, LOG_USER);
+
+    // Create Queue
+    mq = mq_open(QUEUE_NAME, O_CREAT | O_RDONLY, 0644, &queue_attributes);
+
+    do {
+        printf("%s\n\n", "New Loop");
+
+        // Read message
+        ssize_t bytes_read;
+        bytes_read = mq_receive(mq, buffer, MAX_BUF, NULL);
+        buffer[bytes_read] = '\0';
+
+        if (strstr(buffer, "backup")) {
+            // Backup and Transfer
+            printf("%s\n\n", "BACKUP");
+        }
+
+        // Log Status
+        if (!strcmp(buffer, "error")) {
+            syslog(LOG_ERR, "%s", buffer);
+        }
+        else {
+            syslog(LOG_INFO, "%s", buffer);
+        }
+
+        // Terminate Daemon
+        if (!strncmp(buffer, "exit", strlen("exit"))) {
+            terminate = 1;
+        }
+    } while (!terminate);
+
+    mq_close(mq);
+    mq_unlink(QUEUE_NAME);
+    closelog();
 
     return 0;
 }

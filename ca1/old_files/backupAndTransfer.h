036905b974aa97eca9048578eed44ba30579ec73
Vlad Zat <vlad.zat@mydit.ie>
2018-03-17 21:06:12 +0000

    Remove files

--- ca1/old_files/backupAndTransfer.h
+++
@@ -1,6 +0,0 @@
-#ifndef BACKUP_AND_TRANSFER_H
-#define BACKUP_AND_TRANSFER_H
-
-void backupAndTransfer();
-
-#endif // BACKUP_AND_TRANSFER_H
--- ca1/old_files/scheduler.h
+++
@@ -1,6 +0,0 @@
-#ifndef SCHEDULER_H
-#define SCHEDULER_H
-
-void scheduleBackupAndTransfer(int, int, int);
-
-#endif // SCHEDULER_H

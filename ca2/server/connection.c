ebbd1538d861ebf5d1ad38a67231b93e62905559
Vlad Zat <vlad.zat@mydit.ie>
2018-04-24 14:01:08 +0100

    Send credentials

--- ca2/server/connection.c
+++ ca2/server/connection.c
@@ -8,14 +8,52 @@
 
 #include "macros.h"
 
+pthread_mutex_t lock_x;
+
 void *connection(void *arg) {
     int socket = * (int*) arg;
 
+    int authenticated = 0;
     int READSIZE;
     char message[MAX_BUF];
+    char line[MAX_BUF];
 
-    while ((READSIZE = recv(socket, message, 2000, 0)) > 0) {
-        printf("%s", message);
+    char *authMessage = "Authenticated";
+    char *errorAuth = "Invalid credentials";
+
+    // Receive data from the client
+    while ((READSIZE = recv(socket, message, MAX_BUF, 0)) > 0) {
+        // Get files
+        if (authenticated) {
+            printf("Authenticated");
+        }
+
+        // Authenticate user
+        if (!authenticated) {
+            // Block access before accesing the htpasswd file
+            pthread_mutex_lock(&lock_x);
+
+            FILE *fp;
+            fp = fopen(HTPASSWD_PATH, "r");
+
+            while(fgets(line, MAX_BUF, fp)) {
+                printf("%s\n%s", line, message);
+                if (strcmp(line, message) == 0) {
+                    authenticated = 1;
+                    break;
+                }
+            }
+
+            // Unblock mutex
+            pthread_mutex_unlock(&lock_x);
+
+            if (authenticated) {
+                send(socket, authMessage, sizeof(authMessage), 0);
+            }
+            else {
+                send(socket, errorAuth, sizeof(errorAuth), 0);
+            }
+        }
     }
 
     if (READSIZE == 0) {

6b397122943c2f9064e0e4102b4f8ae93a68f151
Vlad Zat <vlad.zat@mydit.ie>
2018-04-24 14:01:18 +0100

    Send Credentials

---
+++ ca2/client/macros.h
@@ -0,0 +1,1 @@
+#define MAX_BUF 1024
--- ca2/client/main.c
+++ ca2/client/main.c
@@ -4,11 +4,15 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
+#include "macros.h"
+
 int main (int argc, char ** argv) {
     int SID;
     struct sockaddr_in server;
-    char clientMessage[500];
-    char serverMessage[500];
+    char username[100];
+    char password[100];
+    char credentials[MAX_BUF];
+    char serverMessage[MAX_BUF];
 
     // Create Socket
     SID = socket(AF_INET, SOCK_STREAM, 0);
@@ -32,21 +36,26 @@ int main (int argc, char ** argv) {
     printf("Successfully connected to the server\n");
 
     while (1) {
-        printf("\nEnter message: ");
-        scanf("%s", clientMessage);
+        printf("\nUsername: ");
+        scanf("%s", username);
+
+        printf("\nPassword: ");
+        scanf("%s", password);
+
+        sprintf(credentials, "%s:%s", username, password);
 
         // Send some data
-        if (send(SID, clientMessage, strlen(clientMessage), 0) < 0) {
+        if (send(SID, credentials, strlen(credentials), 0) < 0) {
             printf("Send failed");
             return 1;
         }
 
         // Receive reply from server
-        // if (recv(SID, serverMessage, 500, 0) < 0) {
-        //     printf("IO error");
-        //     break;
-        // }
-        //
-        // printf("Server sent: %s", serverMessage);
+        if (recv(SID, serverMessage, MAX_BUF, 0) < 0) {
+            printf("IO error");
+            break;
+        }
+
+        printf("Server sent: %s", serverMessage);
     }
 }
